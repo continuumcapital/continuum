cffcc99d1089e9a8f989c6d879c6f95170da6736
Tyler Stober <tylerstober@Tylers-MacBook-Pro.local>
2022-11-01 21:45:05 +0000

    fixes

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,47 +1,47 @@
-// For the scroll position of a page
-// Here we do this to change styling, animations, ect on different scroll positions
+// // For the scroll position of a page
+// // Here we do this to change styling, animations, ect on different scroll positions
 
-export const useScrollPosition = () => {
-  if (typeof window === "undefined") return 500;
+// export const useScrollPosition = () => {
+//   if (typeof window === "undefined") return 500;
 
-  // Store the state
-  const [scrollPos, setScrollPos] = useState(window.pageYOffset);
+//   // Store the state
+//   const [scrollPos, setScrollPos] = useState(window.pageYOffset);
 
-  // On Scroll
-  const onScroll = () => {
-    setScrollPos(window.pageYOffset);
-  };
+//   // On Scroll
+//   const onScroll = () => {
+//     setScrollPos(window.pageYOffset);
+//   };
 
-  // Add and remove the window listener
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  });
+//   // Add and remove the window listener
+//   useEffect(() => {
+//     window.addEventListener("scroll", onScroll);
+//     return () => {
+//       window.removeEventListener("scroll", onScroll);
+//     };
+//   });
   
-  return scrollPos;
-}
+//   return scrollPos;
+// }
 
-export const useScrollDirection = () => {
-  const [ scrollDirection, setScrollDirection ] = useState(null);
+// export const useScrollDirection = () => {
+//   const [ scrollDirection, setScrollDirection ] = useState(null);
 
-  useEffect(() => {
-    let lastScrollY = window.pageYOffset;
+//   useEffect(() => {
+//     let lastScrollY = window.pageYOffset;
 
-    const updateScrollDirection = () => {
-      const scrollY = window.pageYOffset;
-      const direction = scrollY > lastScrollY ? "down" : "up";
-      if (direction !== scrollDirection && (scrollY - lastScrollY > 5 || scrollY - lastScrollY < -5)) {
-        setScrollDirection(direction);
-      }
-      lastScrollY = scrollY > 0 ? scrollY : 0;
-    };
-    window.addEventListener("scroll", updateScrollDirection); // add event listener
-    return () => {
-      window.removeEventListener("scroll", updateScrollDirection); // clean up
-    }
-  }, [scrollDirection]);
+//     const updateScrollDirection = () => {
+//       const scrollY = window.pageYOffset;
+//       const direction = scrollY > lastScrollY ? "down" : "up";
+//       if (direction !== scrollDirection && (scrollY - lastScrollY > 5 || scrollY - lastScrollY < -5)) {
+//         setScrollDirection(direction);
+//       }
+//       lastScrollY = scrollY > 0 ? scrollY : 0;
+//     };
+//     window.addEventListener("scroll", updateScrollDirection); // add event listener
+//     return () => {
+//       window.removeEventListener("scroll", updateScrollDirection); // clean up
+//     }
+//   }, [scrollDirection]);
 
-  return scrollDirection;
-}
+//   return scrollDirection;
+// }
