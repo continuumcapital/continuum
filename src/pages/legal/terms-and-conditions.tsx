8549a628de26f26d7739f7822d8342d852924536
Tyler Stober <tylerstober@Tylers-MacBook-Pro.local>
2022-11-02 15:09:52 +0000

    fixes

--- src/pages/legal/terms-and-conditions.tsx
+++ src/pages/legal/terms-and-conditions.tsx
@@ -32,7 +32,7 @@ const Page: NextPage = () => {
           <List listStyle="bulleted" spacing="l1">
             <li>
               <strong>Affiliate</strong> means an entity that controls, is controlled by or is under common control 
-              with a party, where "control" means ownership of 50% or more of the shares, equity interest or 
+              with a party, where &apos;control&apos; means ownership of 50% or more of the shares, equity interest or 
               other securities entitled to vote for election of directors or other managing authority.
             </li>
 
@@ -80,7 +80,7 @@ const Page: NextPage = () => {
       <Block width="small">
         <Text>
           <h2><Heading size="l4" bold="heavy" title="Acknowledgment" /></h2>
-          
+
           <p>
             These are the Terms and Conditions governing the use of this Service and the agreement that operates between You and the Company. 
             These Terms and Conditions set out the rights and obligations of all users regarding the use of the Service.
@@ -150,7 +150,7 @@ const Page: NextPage = () => {
           <p>
             Notwithstanding any damages that You might incur, the entire liability of the Company and any of its suppliers under any provision 
             of this Terms and Your exclusive remedy for all of the foregoing shall be limited to the amount actually paid by You through the 
-            Service or 100 USD if You haven't purchased anything through the Service.
+            Service or 100 USD if You haven&apos;t purchased anything through the Service.
           </p>
 
           <p>
@@ -164,7 +164,7 @@ const Page: NextPage = () => {
 
           <p>
             Some states do not allow the exclusion of implied warranties or limitation of liability for incidental or consequential damages, 
-            which means that some of the above limitations may not apply. In these states, each party's liability will be limited to the 
+            which means that some of the above limitations may not apply. In these states, each party&apos;s liability will be limited to the 
             greatest extent permitted by law.
           </p>
         </Text>
@@ -175,7 +175,7 @@ const Page: NextPage = () => {
           <h2><Heading size="l4" bold="heavy" title="'AS IS' and 'AS AVAILABLE' Disclaimer" /></h2>
 
           <p>
-            The Service is provided to You "AS IS" and "AS AVAILABLE" and with all faults and defects without warranty of any kind. To the 
+            The Service is provided to You &apos;AS IS&apos; and &apos;AS AVAILABLE&apos; and with all faults and defects without warranty of any kind. To the 
             maximum extent permitted under applicable law, the Company, on its own behalf and on behalf of its Affiliates and its and their 
             respective licensors and service providers, expressly disclaims all warranties, whether express, implied, statutory or otherwise, 
             with respect to the Service, including all implied warranties of merchantability, fitness for a particular purpose, title and 
@@ -187,7 +187,7 @@ const Page: NextPage = () => {
           </p>
 
           <p>
-            Without limiting the foregoing, neither the Company nor any of the company's provider makes any representation or warranty of 
+            Without limiting the foregoing, neither the Company nor any of the company&apos;s provider makes any representation or warranty of 
             any kind, express or implied: &#40;i&#41; as to the operation or availability of the Service, or the information, content, and materials 
             or products included thereon; &#40;ii&#41; that the Service will be uninterrupted or error-free; &#40;iii&#41; as to the accuracy, reliability, 
             or currency of any information or content provided through the Service; or &#40;iv&#41; that the Service, its servers, the content, or 
@@ -240,7 +240,7 @@ const Page: NextPage = () => {
 
           <p>
             You represent and warrant that 	&#40;i&#41; You are not located in a country that is subject to the United States government embargo, or that has been 
-            designated by the United States government as a "terrorist supporting" country, and &#40;ii&#41; You are not listed on any United States 
+            designated by the United States government as a &apos;terrorist supporting&apos; country, and &#40;ii&#41; You are not listed on any United States 
             government list of prohibited or restricted parties.
           </p>
         </Text>
@@ -265,7 +265,7 @@ const Page: NextPage = () => {
 
           <p>
             Except as provided herein, the failure to exercise a right or to require performance of an obligation under these Terms shall not effect a 
-            party's ability to exercise such right or require such performance at any time thereafter nor shall the waiver of a breach constitute a waiver 
+            party&apos;s ability to exercise such right or require such performance at any time thereafter nor shall the waiver of a breach constitute a waiver 
             of any subsequent breach.
           </p>
         </Text>
@@ -288,7 +288,7 @@ const Page: NextPage = () => {
 
           <p>
             We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is material We will make reasonable 
-            efforts to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at 
+            efforts to provide at least 30 days&apos; notice prior to any new terms taking effect. What constitutes a material change will be determined at 
             Our sole discretion.
           </p>
 
